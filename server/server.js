fb617131e8b17abead6728c896971221147696ab
L-u-n-a <j.vannatijne2@hotmail.com>
2017-11-22 17:00:42 +0100

    Can read form data, prints that data to the terminal after sending form in index.html

--- server/server.js
+++ server/server.js
@@ -1,16 +1,27 @@
+/*jshint esversion: 6*/
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const publicPath = path.join(__dirname, '../public/');
 
 var app = express();
 
+// Middleware - Extends express app functionality.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
 app.get('/', function(req, res){
-  res.sendFile(publicPath + 'index.html')
-})
+  res.sendFile(publicPath + 'index.html');
+});
 
 app.get('/about', function(req, res){
-  res.sendFile(publicPath + 'about.html')
-})
+  res.sendFile(publicPath + 'about.html');
+});
+
+app.post('/addAppointment', (req, res) => {
+  console.log(req.body.name);
+  console.log(req.body.date);
+});
 
 app.listen(3000, function (){
   console.log("Connected to server");
